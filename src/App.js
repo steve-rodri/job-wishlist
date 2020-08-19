c28dc2f76bb51bce16817d2c51ccde64af947c00
steverodri91 <steve.rodri@gmail.com>
2020-08-19 14:06:11 -0400

    Setup Main Section

--- src/App.js
+++ src/App.js
@@ -2,7 +2,21 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <main>
+        <header>
+          <div className="Icon">Icon</div>
+          <div className="Title">
+            <h1>WISHLIST</h1>
+            <h2>Jobs</h2>
+          </div>
+        </header>
+        <button>+</button>
+        <div className="List"></div>
+      </main>
+    </div>
+  );
 }
 
 export default App;
